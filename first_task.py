38d16f33cb90d02050816e5f5843d0852a33034e
Ilya <lukin.ilya.m@gmail.com>
2022-06-03 18:34:13 +0300

    Доработка

--- first_task.py
+++ first_task.py
@@ -13,6 +13,9 @@ for sentence in stext:
     else:
         sentences.append(sentence)
 print(sentences)
+print(len(stext))
+print(stext)
+
 
 for sentence in sentences:
     for pos in range(len(alphabet)):
@@ -25,3 +28,25 @@ for sentence in sentences:
             else:
                 print(letter, end='')
         print('')
+keys = [17, 8, 12, 4, 7, 7, 9, 13, 20]
+
+answer = ''
+
+def shifr(text, key):
+    global answer
+    for letter in text:
+        if letter in alphabet:
+            answer += alphabet[(alphabet.index(letter) - key) % len(alphabet)]
+        elif letter in ALPHABET:
+            answer += ALPHABET[(ALPHABET.index(letter) - key) % len(ALPHABET)]
+        else:
+            answer += letter
+
+
+for i in range(len(stext)):
+    if i == 0 or i == 1:
+        shifr(stext[i], keys[0])
+    else:
+        shifr(stext[i], keys[i-1])
+# вопросительные предложения заменить вручную и все
+print(answer)
