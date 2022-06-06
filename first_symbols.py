3b7ab3dd963bbedacc5760132d89c2eafb0dfb55
Ilya <lukin.ilya.m@gmail.com>
2022-06-06 23:07:36 +0300

    Второй спринт второе задание

---
+++ first_symbols.py
@@ -0,0 +1,8 @@
+with open('Лукин_Илья_Спринт1.txt') as a:
+    if a.read(2) == 'MZ':
+        print('0x4D 0x5A, MZ - executable, OS Windows')
+    else:
+        a.seek(0, 0)
+        symbols = a.read(2)
+        print(f'{hex(ord(symbols[0]))} {hex(ord(symbols[1]))}  {symbols} - non-executable')
+
